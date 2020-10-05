62e2e3e6c5826da7ddd5e9e31733232fee750942
jensen-yan <yanyue171@mails.ucas.ac.cn>
2020-10-05 16:04:57 +0800

    修改了am中dummy访问串口地址导致访问越界的问题

--- nexus-am/am/src/nutshell/common/uartlite.c
+++ nexus-am/am/src/nutshell/common/uartlite.c
@@ -11,18 +11,18 @@
 #define UARTLITE_RX_VALID 0x01
 
 void __am_init_uartlite(void) {
-  outb(UARTLITE_MMIO + UARTLITE_CTRL_REG, UARTLITE_RST_FIFO);
+//  outb(UARTLITE_MMIO + UARTLITE_CTRL_REG, UARTLITE_RST_FIFO);
 }
 
 void __am_uartlite_putchar(char ch) {
-  if (ch == '\n') __am_uartlite_putchar('\r');
+//  if (ch == '\n') __am_uartlite_putchar('\r');
 
-  while (inb(UARTLITE_MMIO + UARTLITE_STAT_REG) & UARTLITE_TX_FULL);
-  outb(UARTLITE_MMIO + UARTLITE_TX_FIFO, ch);
+//  while (inb(UARTLITE_MMIO + UARTLITE_STAT_REG) & UARTLITE_TX_FULL);
+//  outb(UARTLITE_MMIO + UARTLITE_TX_FIFO, ch);
 }
 
 int __am_uartlite_getchar() {
-  if (inb(UARTLITE_MMIO + UARTLITE_STAT_REG) & UARTLITE_RX_VALID)
-    return inb(UARTLITE_MMIO + UARTLITE_RX_FIFO);
+//  if (inb(UARTLITE_MMIO + UARTLITE_STAT_REG) & UARTLITE_RX_VALID)
+//    return inb(UARTLITE_MMIO + UARTLITE_RX_FIFO);
   return 0;
 }
